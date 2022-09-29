f379b68c2b30cd3872e05a7339b5d7adc4a0a37f
Nicolas Almonacid <nalmonacidojeda@gmail.com>
2022-09-29 02:00:12 -0300

    Fin seccion 14

--- 05 - heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05 - heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,14 +1,111 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
 @Component({
   selector: 'app-agregar',
-  templateUrl: './agregar.component.html'
+  templateUrl: './agregar.component.html',
+  styles:[`
+  
+  img{
+    width: 100%;
+    border-radius: 5px;
+  }
+  
+  `]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+
+
+  }
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog ) { }
 
   ngOnInit(): void {
+
+if(!this.router.url.includes('editar')){
+return
+}
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.heroesService.getHeroePorId(id))
+    )
+    .subscribe(heroe => this.heroe = heroe);
+
   }
 
+  guardar(){
+    console.log(this.heroe);
+    if(this.heroe.superhero.trim().length === 0){
+      return;
+    }
+
+    if(this.heroe.id){
+      //actualizar
+      this.heroesService.actualizarHeroe(this.heroe).subscribe(heroe => this.mostrarSnakbar('Registro Actualizado'))
+    }else{
+      //agregar
+      this.heroesService.agregarHeroe(this.heroe).subscribe(heroe => {
+       this.router.navigate(['/heroes/editar',heroe.id]);
+       this.mostrarSnakbar('Registro Creado');
+      })
+    }
+  
+  }
+
+  borrarHeroe(){
+
+    const dialog =this.dialog.open(ConfirmarComponent,
+      {
+        width: '550px',
+        data: this.heroe
+      });
+
+dialog.afterClosed().subscribe(result => {
+  if(result){
+    this.heroesService.BorrarHeroe(this.heroe.id!).subscribe(resp => {
+
+      this.router.navigate(['/heroes']);
+    });
+  }
+})
+
+
+  }
+
+  mostrarSnakbar(mensaje: string){
+
+    this.snackBar.open(mensaje, 'Cerrar',{
+      duration: 2500
+    })
+  }
 }
--- 05 - heroesApp/src/app/heroes/pipes/imagen.pipe.ts
+++ 05 - heroesApp/src/app/heroes/pipes/imagen.pipe.ts
@@ -2,13 +2,22 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { Heroe } from "../interfaces/heroes.interface";
 
 @Pipe({
-    name: 'imagen'
+    name: 'imagen',
+    pure: false
 })
 export class ImagenPipe implements PipeTransform{
    
 transform(valor: Heroe) {
    
-return `assets/heroes/${valor.id}.jpg`;
+if(!valor.id && !valor.alt_img){
+    return `assets/no-image.png`;
+}else if(valor.alt_img){
+    return valor.alt_img
+}else{
+
+    return `assets/heroes/${valor.id}.jpg`;
+}
+
 
    
    
