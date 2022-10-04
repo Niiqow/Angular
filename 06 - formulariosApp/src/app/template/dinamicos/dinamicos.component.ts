21b7119d2d1fb3b913f124617d9bdb65732ed905
Nicolas Almonacid <nalmonacidojeda@gmail.com>
2022-10-03 21:45:42 -0300

    fin seccion 16

---
+++ 06 - formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[];  
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
+
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent {
+
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Nicolas',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Los Sims'
+      },
+      {
+        id: 2,
+        nombre: 'League of Legends'
+      }
+    ]
+  }
+
+  constructor() { }
+
+eliminar(index: number){
+this.persona.favoritos.splice(index,1);
+}
+
+agregarJuego(){
+  const nuevoFavorito: Favorito = {
+
+    id: this.persona.favoritos.length + 1,
+    nombre: this.nuevoJuego
+
+  }
+  this.persona.favoritos.push({...nuevoFavorito});
+  this.nuevoJuego = '';
+}
+  guardar(){
+
+    console.log('formulario posteado');
+
+  }
+
+
+
+}
