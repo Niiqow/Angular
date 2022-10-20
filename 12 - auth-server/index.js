a227236720e4364a71fc87c2aadc499e0a04b8da
Nicolas Almonacid <nalmonacidojeda@gmail.com>
2022-10-20 18:28:08 -0300

    Fin del curso

--- 12 - auth-server/index.js
+++ 12 - auth-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -27,6 +28,12 @@ app.use(express.json());
 //rutas
 
 app.use( '/api/auth', require('./routes/auth') );
+
+//manejar demas rutas
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`servidor corriendo el puerto ${4000}`)
